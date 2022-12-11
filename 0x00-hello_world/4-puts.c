f5606c74c1348077421c033d00db6ee7cc311916
neemangotho <ngothoneema@gmail.com>
2022-12-11 01:28:32 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
-#include <stdio.h>                                                                                                                      
-/**                                                                                                                                     
- *  * main - Entry                                                                                                                         
- *   * Return: Always 0                                                                                                                     
- *    */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-	        puts("\"Programming is like building a multi lingual puzzle);                                                            
-			        return (0);
+#include <stdio.h>
+/**
+ * main - Entry
+ * return:Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
 }
 
 
